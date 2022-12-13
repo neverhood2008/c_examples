ae80673b424ea5e3a5f0a40113108f8bfb894894
Elena S <neverhood_2008@mail.ru>
2022-12-14 02:23:46 +0300

    задача5

---
+++ Examples_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя  ");
+string username=Console.ReadLine();
+if (username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
