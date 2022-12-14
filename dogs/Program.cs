7f53d52e33da9b522d67b689051db4dcc3a22ce9
Elena S <neverhood_2008@mail.ru>
2022-12-14 23:02:36 +0300

    Задача о собаках

---
+++ dogs/Program.cs
@@ -0,0 +1,23 @@
+﻿int count=0;
+int distance=10000;
+int firstfriendspeed=1;
+int secondfriendspeed=2;
+int dogspeed=5;
+int friend=2;
+while (distance>10)
+{
+    if (friend==1)
+    {
+        Time=distance/(firstfriendspeed+dogspeed);
+        friend=2;
+    }
+    else
+    {
+         Time=distance/(secondfriendspeed+dogspeed);
+         friend=1;
+    }   
+    }
+    distance=distance-(firstfriendspeed+secondfriendspeed)*Time;
+    count=count+1
+}
+Console.WriteLine("собака пробежит ", count , " раз");
